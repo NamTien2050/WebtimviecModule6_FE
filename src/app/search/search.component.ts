e1f5ecfd2ac3ecb0b8f4919be0ea072073fcb6b6
Nam <nguyenvuhoangthien2050@gmail.com>
2021-11-10 23:20:08 +0700

    Tìm Kiếm nhiều trường

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,27 +1,35 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {RecruitmentPostService} from "../service/recruitment-post.service";
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 
-export class SearchComponent implements OnInit{
+export class SearchComponent implements OnInit {
   field: any;
   findField: string = '';
   formGroupFind!: FormGroup;
-  @Output() autoSearch: EventEmitter<string>= new EventEmitter<string>();
-  @Output() groupFilters: EventEmitter<any>= new EventEmitter<any>();
+  @Output() autoSearch: EventEmitter<string> = new EventEmitter<string>();
+  @Output() groupFilters: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(private RecruitmentPostService: RecruitmentPostService,
               private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.formGroupFind = new FormGroup({
-      findField: new FormControl("")
-    })
+    this.buildForm()
+    // this.formGroupFind = new FormGroup({
+    //   findField: new FormControl("")
+    // })
+  }
+
+  buildForm(): void {
+    this.formGroupFind = this.formBuilder.group({
+      search: new FormControl(''),
+    });
   }
 
   findAllByFieldContaining() {
@@ -30,4 +38,16 @@ export class SearchComponent implements OnInit{
       console.log(this.field);
     });
   }
+
+ findAllByFieldContaining2() {
+    this.RecruitmentPostService.findAllByFieldContaining2(this.formGroupFind.get('search')?.value).subscribe((field) => {
+      this.field = field;
+      console.log(this.field);
+    });
+  }
+
+  // Search(filters: any): void {
+  //   Object.keys(filters).forEach(key => filters[key] === '' ? delete filters[key] : key);
+  //   this.groupFilters.emit(filters);
+  // }
 }
--- src/app/service/recruitment-post.service.ts
+++ src/app/service/recruitment-post.service.ts
@@ -8,12 +8,17 @@ import {RecruitmentPost} from "../model/RecruitmentPost";
   providedIn: 'root'
 })
 export class RecruitmentPostService {
-  RecruitmentPost: RecruitmentPost[]=[];
+  RecruitmentPost: RecruitmentPost[] = [];
+
   constructor(private http: HttpClient) {
   }
 
   findAllByFieldContaining(field: string) {
     return this.http.get(`${environment.apiUrl}/post/fields/` + field)
+  }
+
+  findAllByFieldContaining2(search: string) {
+    return this.http.get(`${environment.apiUrl}/post/searchAdvanced?search=` + search)
   }
 
 }
